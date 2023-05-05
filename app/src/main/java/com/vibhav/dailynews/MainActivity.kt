c9c10534b738e2f70104e1dcd9013a731996a8df
Vibhav <vibhavsachan743@gmail.com>
2023-05-05 18:55:25 +0530

    Item view created

--- app/src/main/java/com/vibhav/dailynews/MainActivity.kt
+++ app/src/main/java/com/vibhav/dailynews/MainActivity.kt
@@ -2,10 +2,32 @@ package com.vibhav.dailynews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getNews()
+    }
+
+    private fun getNews() {
+        val news: Call<News> = NewsService.newsInstance.getHeadLines("in",1)
+        news.enqueue(object: Callback<News>{
+            override fun onResponse(call: Call<News>, response: Response<News>) {
+                val news:News? = response.body()
+                if(news != null)
+                {
+                    Log.d("Good",news.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<News>, t: Throwable) {
+                Log.d("Good","Error in fetching News",t)
+            }
+        })
     }
 }
